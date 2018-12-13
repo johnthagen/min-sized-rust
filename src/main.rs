02b2312dbefeb1476e20ff9aead3bbf80f9fa26a
johnthagen <johnthagen@gmail.com>
2018-12-13 17:38:38 -0500

    Fix accidental reintroduction of jemalloc

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 // Use the system allocator instead of bundling
 // jemalloc into the binary.
-//use std::alloc::System;
-//
-//#[global_allocator]
-//static A: System = System;k
+use std::alloc::System;
+
+#[global_allocator]
+static A: System = System;
 
 fn main() {
     println!("Hello, world!");
