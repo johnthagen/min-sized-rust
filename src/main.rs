642267542c6f08ebfd8a930eb43c478dd0a0917a
johnthagen <johnthagen@gmail.com>
2019-02-20 16:55:26 -0500

    Remove setting system allocator as Rust 1.32+ do this by default

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-// Use the system allocator instead of bundling
-// jemalloc into the binary.
-use std::alloc::System;
-
-#[global_allocator]
-static A: System = System;
-
 fn main() {
     println!("Hello, world!");
 }
